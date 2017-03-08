510497edd9f9d2046fbdaa416d278c21e6e6f3fb
Ante Wall <antewall@gmail.com>
2017-03-08 21:29:20 +0100

    Can filter and select event

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { LoginService } from './../services';
 
 @Component({
   selector: 'topbar',
@@ -8,70 +9,23 @@ import { FormControl } from '@angular/forms';
 })
 export class TopbarComponent {
   private eventCtrl: FormControl;
-  filteredStates: any;
+  private events: Array<any>;
+  filteredEvents: any;
 
-  constructor() {
+  constructor(private loginService: LoginService) {
     this.eventCtrl = new FormControl();
-    this.filteredStates = this.eventCtrl.valueChanges
+    this.filteredEvents = this.eventCtrl.valueChanges
       .startWith(null)
-      .map(name => this.filterStates(name));
+      .map(name => this.filterEvents(name));
+    this.loginService.profile.subscribe((profile) => {
+      if (profile && profile.events) {
+        this.events = profile.events;
+      }
+    });
   }
 
-  states = [
-    'Alabama',
-    'Alaska',
-    'Arizona',
-    'Arkansas',
-    'California',
-    'Colorado',
-    'Connecticut',
-    'Delaware',
-    'Florida',
-    'Georgia',
-    'Hawaii',
-    'Idaho',
-    'Illinois',
-    'Indiana',
-    'Iowa',
-    'Kansas',
-    'Kentucky',
-    'Louisiana',
-    'Maine',
-    'Maryland',
-    'Massachusetts',
-    'Michigan',
-    'Minnesota',
-    'Mississippi',
-    'Missouri',
-    'Montana',
-    'Nebraska',
-    'Nevada',
-    'New Hampshire',
-    'New Jersey',
-    'New Mexico',
-    'New York',
-    'North Carolina',
-    'North Dakota',
-    'Ohio',
-    'Oklahoma',
-    'Oregon',
-    'Pennsylvania',
-    'Rhode Island',
-    'South Carolina',
-    'South Dakota',
-    'Tennessee',
-    'Texas',
-    'Utah',
-    'Vermont',
-    'Virginia',
-    'Washington',
-    'West Virginia',
-    'Wisconsin',
-    'Wyoming',
-  ];
-
-  filterStates(val: string) {
-    return val ? this.states.filter((s) => new RegExp(val, 'gi').test(s)) : this.states;
+  filterEvents(val: string) {
+    console.log(val);
+    return val ? this.events.filter((s) => new RegExp(val, 'gi').test(s.name)) : this.events;
   }
-
 }
